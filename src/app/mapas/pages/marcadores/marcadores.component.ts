19f45102c8358e43adc27dfdabbc4e92e0dbc546
Fabio <fabioaoad@gmail.com>
2021-11-12 11:20:19 -0300

    Mapas - Marcadores en el mapa - Seccion 21 - apartado 15

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import * as mapboxgl from "mapbox-gl";
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+      .mapa-container{
+        height: 100%;
+        width: 100%;
+      }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+
+
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number,number] = [ -64.24425212451473, -27.73596197595897 ];
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    //Aparece "OPTIMUS PRIME" en el mapa
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'OPTIMUS PRIME';
+
+    const marker = new mapboxgl.Marker({
+     // element: markerHtml
+    })
+      .setLngLat( this.center  )
+      .addTo( this.mapa );
+
   }
 
 }
