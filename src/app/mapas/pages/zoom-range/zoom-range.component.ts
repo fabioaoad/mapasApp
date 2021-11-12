e1a6a9664f273007a10e14a039da78a73f4c2520
Fabio <fabioaoad@gmail.com>
2021-11-12 10:05:32 -0300

    Mapas - zoom range de bootstrap - Seccion 21 - apartado 9

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from "mapbox-gl";
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+      .mapa-container{
+        height: 100%;
+        width: 100%;
+      }
+
+      .row{
+        background-color: white;
+        border-radius: 5px;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        position: fixed;
+        z-index: 999;
+      }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +28,11 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -64.24425212451473, -27.73596197595897 ],
+      zoom: 18
+    });
   }
-
 }
